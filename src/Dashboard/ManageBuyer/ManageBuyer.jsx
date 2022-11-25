1d9e5be109a30d79370709d8ca0fa2d5ce8293d9
delower68 <hdelower68@gmail.com>
2022-11-26 02:45:38 +0800

    make a admin done

--- src/Dashboard/ManageBuyer/ManageBuyer.jsx
+++ src/Dashboard/ManageBuyer/ManageBuyer.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import toast from "react-hot-toast";
 import Spinner from "../../pages/Spinner/Spinner";
 
 const ManageBuyer = () => {
   const url = "http://localhost:8000/users";
-  const { data: users = [], isLoading } = useQuery({
+  const { data: users = [],refetch, isLoading } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await fetch(url);
@@ -12,6 +13,20 @@ const ManageBuyer = () => {
       return data;
     },
   });
+
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:8000/users/admin/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.modifiedCount > 0){
+            toast.success("Make admin successfully")
+            refetch()
+        }
+      });
+  };
 
   if (isLoading) {
     return <Spinner></Spinner>;
@@ -29,6 +44,8 @@ const ManageBuyer = () => {
                 <th>Name</th>
                 <th>Email</th>
                 <th>User Type</th>
+                <th>Admin </th>
+                <th>Action </th>
               </tr>
             </thead>
             <tbody>
@@ -38,6 +55,26 @@ const ManageBuyer = () => {
                   <td>{user?.name}</td>
                   <td>{user?.email}</td>
                   <td>{user?.type}</td>
+
+                  <td>
+                    {user?.type !== "admin" && (
+                      <button
+                        onClick={() => handleMakeAdmin(user._id)}
+                        className="btn btn-xs bg-green-400"
+                      >
+                        Make Admin
+                      </button>
+                    )}
+                  </td>
+
+                  <td>
+                    <label
+                      htmlFor="confirmation-modal"
+                      className="bg-red-400 btn  btn-sm"
+                    >
+                      Delete
+                    </label>
+                  </td>
                 </tr>
               ))}
             </tbody>
